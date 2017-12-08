55a1f23fb292504643cf51be3356e3876eb27d2d
JR <jan.ruzicka.vsb@gmail.com>
2017-12-08 10:49:00 +0100

    Initial commit

---
+++ src/main/java/com/example/android/networkconnect/MessageFile.java
@@ -0,0 +1,14 @@
+package com.example.android.networkconnect;
+
+/**
+ * Created by jencek on 27.10.17.
+ */
+
+public class MessageFile {
+    String message;
+    String filename;
+    public MessageFile(String message, String filename) {
+        this.message = message;
+        this.filename = filename;
+    }
+}
